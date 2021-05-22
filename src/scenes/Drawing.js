708549767c9888db634ad198bbaaaa59e5c07532
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-05-22 11:48:51 -0700

    Added in brush and pencil sprites

--- src/scenes/Drawing.js
+++ src/scenes/Drawing.js
@@ -10,6 +10,8 @@ class Drawing extends Phaser.Scene{
     preload(){
         this.load.image('BG1', './assets/floor1BG.png');
         this.load.image('player', './assets/Detective Doggert 001.png');
+        this.load.image('brush', './assets/testBrush.png');
+        this.load.image('brush', './assets/testPencil.png');
     }
     
     create ()
@@ -17,11 +19,11 @@ class Drawing extends Phaser.Scene{
         this.background = this.add.image(game.config.width/2, game.config.height/2, 'BG1');
         this.rt = this.add.renderTexture(0,0,960,720);
 
-        //the 32's depend on the sprite size
+        //the 32's depend on the sprite size and screen size
         this.input.on('pointermove', function (pointer) {
             if (pointer.isDown)
             {
-                this.rt.draw('player', pointer.x - 32, pointer.y - 32);
+                this.rt.draw('brush', pointer.x - 32, pointer.y - 32);
             }
         }, this);
         //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.RenderTexture.html#clear__anchor   graphics stuff
