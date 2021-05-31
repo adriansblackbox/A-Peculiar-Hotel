72b9b0f9e4a8663538df8d591fe75b7b9ec8f587
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-05-30 17:06:47 -0700

    my thing isn't updating again

--- src/prefabs/Monster.js
+++ src/prefabs/Monster.js
@@ -1,6 +1,6 @@
 class Monster extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene,x,y,texture,frame, speedP){
-        super(scene,x,y,texture,frame, speedP);
+    constructor(scene,x,y,texture,frame, speedP, levelP){
+        super(scene,x,y,texture,frame, speedP, levelP);
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
@@ -10,6 +10,7 @@ class Monster extends Phaser.Physics.Arcade.Sprite{
 
         this.startingX = this.x;
         this.startingY = this.y;
+        this.level = levelP;
         this.speed = speedP; //58
         this.direction = false;
         this.moveList = [];
@@ -22,49 +23,52 @@ class Monster extends Phaser.Physics.Arcade.Sprite{
         this.onRIGHT = false;
     }
     update(playerX, playerY){
-        //console.log("thisX: " , this.x);
+        console.log("thisX: " , this.x);
         //console.log("playerX: ",playerX);
         this.movement(playerX, playerY);
     }
 
     movement(pX, pY){
-        if(this.x > pX - 1 && this.x < pX + 1)
-        {
-            this.setVelocityX(0);
+        if(this.levelP == 1){
+            //back and fourth movement based on starting position
+            if(this.x < this.startingX + 100 && this.direction == false){
+                this.setVelocity(this.speed, 0);
+                console.log("if statement");
+                if(this.x > this.startingX + 90){
+                    this.direction = true;
+                }
+            }
+            else if(this.x > this.startingX - 100 && this.direction == true){
+                this.setVelocity(-this.speed,0);
+                if(this.x < this.startingX - 90){
+                    this.direction = false;
+                }
+            }
         }
-        else if(this.x > pX){
-            this.setVelocityX(-this.speed);
-        }
-        else if(this.x < pX){
-            this.setVelocityX(this.speed);
-        }
-        if(this.y > pY - 1 && this.y < pY + 1)
-        {
-            this.setVelocityY(0);
-        }
-        else if(this.y > pY){
-            this.setVelocityY(-this.speed);
-        }
-        else if(this.y < pY){
-            this.setVelocityY(this.speed);
-        }
-
-        /*
         
-        //back and fourth movement based on starting position
-        if(this.x < this.startingX + 100 && this.direction == false){
-            this.setVelocity(this.speed, 0);
-            console.log("if statement");
-            if(this.x > this.startingX + 90){
-                this.direction = true;
+        if(this.levelP == 4){
+            if(this.x > pX - 1 && this.x < pX + 1)
+            {
+                this.setVelocityX(0);
+            }
+            else if(this.x > pX){
+                this.setVelocityX(-this.speed);
+            }
+            else if(this.x < pX){
+                this.setVelocityX(this.speed);
+            }
+            if(this.y > pY - 1 && this.y < pY + 1)
+            {
+                this.setVelocityY(0);
+            }
+            else if(this.y > pY){
+                this.setVelocityY(-this.speed);
+            }
+            else if(this.y < pY){
+                this.setVelocityY(this.speed);
             }
         }
-        else if(this.x > this.startingX - 100 && this.direction == true){
-            this.setVelocity(-this.speed,0);
-            if(this.x < this.startingX - 90){
-                this.direction = false;
-            }
-        }
-        */
+        
+        
     }
 }
