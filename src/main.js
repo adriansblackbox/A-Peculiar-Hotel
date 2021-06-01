729fd04d87c5f8bca2488b3f6ff5e8b6b52e438e
Adrian <adlvasqu@ucsc.com>
2021-06-01 12:12:38 -0700

    changes to floor 1 and 4

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ let config = {
     width: 640,
     height: 320,
     pixelArt: true,
-    scene: [Floor_1, Menu, Elevator, Floor_4, Floor_3, Floor_2, Lobby, Floor_1_OTHER, Floor_2_OTHER, Floor_3_OTHER, Floor_4_OTHER, Drawing, Floor_0],
+    scene: [Floor_2, Menu, Elevator, Floor_4, Floor_3, Floor_1, Lobby, Floor_1_OTHER, Floor_2_OTHER, Floor_3_OTHER, Floor_4_OTHER, Drawing, Floor_0],
 
     //3 is ballroom, 1 is square patrolling ghost, 2 is long corridor, 4 is following ghosts
     // variables for states in game
