cc21a184a34da93ef597dc622d7704f5f8e5a23b
bdvictor <bdvictor@ucsc.edu>
2021-06-04 15:57:25 -0700

    rough dialogue box

--- src/scenes/Elevator.js
+++ src/scenes/Elevator.js
@@ -1,6 +1,7 @@
 const COLOR_PRIMARY = 0x4e342e;
 const COLOR_LIGHT = 0x7b5e57;
 const COLOR_DARK = 0x260e04;
+var dialogue1 = "this is the first dialogue with cat";
 
 class Elevator extends Phaser.Scene{
 
@@ -50,7 +51,7 @@ class Elevator extends Phaser.Scene{
 
         this.elevator_bgm = this.sound.add('elevatorMusic', BGMConfig);
         this.musicplaying = false;
-        
+
 
         this.elevatorTime = 5000;
 
@@ -68,7 +69,17 @@ class Elevator extends Phaser.Scene{
 
         this.dogDialogue = this.add.sprite(game.config.width/2, game.config.height/2, 'dogDialogue', 0);
         this.catDialogue = this.add.sprite(game.config.width/2, game.config.height/2, 'catDialogue', 0);
+        this.dialogueFinished1 = false;
+        this.dialogueFinished2 = false;
+        this.dialogueFinished3 = false;
+        this.dialogueFinished4 = false;
 
+        if(!this.dialogueFinished1){
+            createTextBox(this, 100, 100, {wrapWidth: 500,}).start(dialogue1, 100);
+        }
+        if(this.dialogueFinished1 && !this.dialogueFinished2){
+
+        }
 
         console.log(this.password);
         this.nextFloorPressed = false;
@@ -211,4 +222,72 @@ class Elevator extends Phaser.Scene{
             this.scene.switch('Drawing');
         }
     }
+}
+
+const GetValue = Phaser.Utils.Objects.GetValue;
+var createTextBox = function (scene, x, y, config) {
+    var wrapWidth = GetValue(config, 'wrapWidth', 0);
+    var fixedWidth = GetValue(config, 'fixedWidth', 0);
+    var fixedHeight = GetValue(config, 'fixedHeight', 0);
+    var textBox = scene.rexUI.add.textBox({ // adding the text box function 
+            x: x,
+            y: y,
+
+            background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_PRIMARY)
+                .setStrokeStyle(2, COLOR_LIGHT),
+
+            icon: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_DARK),
+
+            text: scene.add.bitmapText(0, 0, 'gothic').setFontSize(20).setMaxWidth(wrapWidth),
+
+            action: scene.add.image(0, 0, 'nextPage').setTint(COLOR_LIGHT).setVisible(false),
+
+            space: {
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 20,
+                icon: 10,
+                text: 10,
+            },
+      
+            page: {
+                maxLines: 3
+            }
+        })
+        .setOrigin(0)
+        .layout();
+
+    textBox
+        .setInteractive()
+        .on('pointerdown', function () {
+            var icon = this.getElement('action').setVisible(false);
+            this.resetChildVisibleState(icon);
+            if (this.isTyping) {
+                this.stop(true);
+            } else {
+                this.typeNextPage();
+            }
+        }, textBox)
+        .on('pageend', function () {
+            if (this.isLastPage) {
+                return;
+            }
+
+            var icon = this.getElement('action').setVisible(true);
+            this.resetChildVisibleState(icon);
+            icon.y -= 30;
+            var tween = scene.tweens.add({
+                targets: icon,
+                y: '+=30', // '+=100'
+                ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+                duration: 500,
+                repeat: 0, // -1: infinity
+                yoyo: false
+            });
+        }, textBox)
+    //.on('type', function () {
+    //})
+
+    return textBox;
 }
