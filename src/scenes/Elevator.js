ec89b194a319fee28f7d7712ecbfcb5f4ad5c0a1
bdvictor <bdvictor@ucsc.edu>
2021-06-06 22:22:28 -0700

    still stuck on floors that have already happened

--- src/scenes/Elevator.js
+++ src/scenes/Elevator.js
@@ -156,6 +156,7 @@ class Elevator extends Phaser.Scene{
         this.conversationDone = false;
         this.keySequenceSetUp = false; 
         this.dialogueSetUp = false;
+        this.refresh = false; 
 
         this.button1_clicked = false;
         this.button2_clicked = false;
@@ -239,16 +240,51 @@ class Elevator extends Phaser.Scene{
         this.Conversation = createTextBox(this, 100, 210, {wrapWidth: 500,});
     }
     update(time, delta){
+        if(this.floorList.length >= 3){
+            if(!this.refresh){
+                this.dialogue_1_InProgress = false;
+                this.dialogue_1_End = false;
+                this.dialogue_2_InProgress = false;
+                this.dialogue_1_End = false;
+                this.dialogue_3_InProgress = false;
+                this.dialogue_1_End = false;
+                this.dialogue_4_InProgress = false;
+                this.dialogue_1_End = false;
+                this.dialogue_5_InProgress = false;
+                this.dialogue_1_End = false;
+
+                this.button1_clicked = false;
+                this.button2_clicked = false;
+                this.button3_clicked = false;
+                this.button4_clicked = false;
+                this.button5_clicked = false;
+                this.button6_clicked = false;
+                this.button7_clicked = false;
+                this.button8_clicked = false;
+                this.button9_clicked = false;
+                this.button10_clicked = false;
+            
+                this.confirmPassword = false;
+                this.musicplaying = false;
+                this.fadingOut = false;
+
+                this.index = -1;
+                this.failedPassword = false;
+                this.correctPassword = false;
+                this.resetGame = true;
+                this.conversationDone = false;
+                this.keySequenceSetUp = false; 
+                this.dialogueSetUp = false;
+                this.refresh = true; 
+            }
+        }
+        
         if(!(this.musicplaying) && !this.fadingOut){
             this.musicplaying = true;
             this.elevator_bgm.play();
         }
         this.elevatorTime += delta;
         this.elevatorScene.anims.play('elevatorScene', true);
-        
-        if(this.floorList.length >= 3 ){
-
-        }
 
 
         if(this.conversationDone && this.dialogue_5_End && !this.keySequenceSetUp){
@@ -638,6 +674,7 @@ class Elevator extends Phaser.Scene{
         }
         if(this.failedPassword && this.resetGame){
             this.resetGame = false;
+            this.refresh = false;
             this.cameras.main.fadeOut(1500, 0, 0, 0);
             this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
                 this.scene.start('Lobby');
